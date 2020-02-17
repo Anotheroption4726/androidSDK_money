4c949ebc694c2990e6babb441d4bea8a62e35293
Mouth <adrien.charneau@edu.itescia.fr>
2020-02-17 13:56:23 +0100

    about activity 2

--- app/src/main/java/com/adrien/money/MainActivity.java
+++ app/src/main/java/com/adrien/money/MainActivity.java
@@ -2,8 +2,8 @@ package com.adrien.money;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,9 +18,10 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button convertBtn = findViewById(R.id.convertButton);
+
         final EditText inputEurValue = findViewById(R.id.euroInput);
         final TextView displayKzt = findViewById(R.id.kztView);
+        final Button convertBtn = findViewById(R.id.convertButton);
 
         convertBtn.setOnClickListener(new View.OnClickListener()
         {
@@ -28,9 +29,45 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View v)
             {
                 String inputTextEurValue = inputEurValue.getText().toString();
+                float inputEuro = Float.parseFloat(inputTextEurValue);
+                float convertedKztValue = convertKzt(inputEuro);
                 //Log.i("MainActivity", inputTextEurValue);
-                displayKzt.setText(inputTextEurValue + " ₸");
+                displayKzt.setText(convertedKztValue + " ₸");
+            }
+        });
+
+
+
+        /////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+        final Button aboutButton = findViewById(R.id.aboutButton);
+
+        aboutButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
             }
         });
     }
+
+
+
+
+
+
+
+
+
+    private float convertKzt(float value)
+    {
+        return value * 408.48f; // euro to kzt conversion rate
+    }
+
 }
