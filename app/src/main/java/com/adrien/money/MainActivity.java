36198158c3c1ac9b86bda7d651dc0b8becb64ddf
Mouth <adrien.charneau@edu.itescia.fr>
2020-02-17 10:51:42 +0100

    setup activity main xml

--- app/src/main/java/com/adrien/money/MainActivity.java
+++ app/src/main/java/com/adrien/money/MainActivity.java
@@ -1,7 +1,9 @@
 package com.adrien.money;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "Hello World!");
     }
 }
