b5205ab5863bfed60bf21d3ee9e94c1a98ef6748
Mouth <adrien.charneau@edu.itescia.fr>
2020-02-17 11:44:22 +0100

    affichage input dans la console

--- app/src/main/java/com/adrien/money/MainActivity.java
+++ app/src/main/java/com/adrien/money/MainActivity.java
@@ -4,14 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    //final Button convertBtn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button convertBtn = findViewById(R.id.convertButton);
+        final EditText inputEurValue = findViewById(R.id.euroInput);
 
-        Log.i("MainActivity", "Hello World!");
+        convertBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String inputTextEurValue = inputEurValue.getText().toString();
+                Log.i("MainActivity", inputTextEurValue);
+            }
+        });
     }
 }
