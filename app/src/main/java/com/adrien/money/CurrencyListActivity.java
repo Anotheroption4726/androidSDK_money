78c5475d7f12d8d07e7f000fe310833de7192da9
Mouth <adrien.charneau@edu.itescia.fr>
2020-02-18 14:13:34 +0100

    debut recyclerview

---
+++ app/src/main/java/com/adrien/money/CurrencyListActivity.java
@@ -0,0 +1,14 @@
+package com.adrien.money;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrencyListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency_list);
+    }
+}
